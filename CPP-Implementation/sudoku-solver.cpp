f08ca797039ae5c33ac45db04d1dfda2e511b8f8
Ryan Mueller <55604547+ryanmueller28@users.noreply.github.com>
2020-03-07 16:18:23 -0600

    sudoku-solver.cpp
    The cpp main program to run the sudoku solver.

---
+++ CPP-Implementation/sudoku-solver.cpp
@@ -0,0 +1,15 @@
+/**
+ * A simple sudoku program that implements "Sudoku.h"
+ * The program was built this way in order to translate the class
+ * File from C++ to Java.
+**/
+#include <iostream>
+#include <cstdlib>
+#include "Sudoku.h"
+
+int main()
+{
+    Sudoku s;
+
+    return EXIT_SUCCESS;
+}
